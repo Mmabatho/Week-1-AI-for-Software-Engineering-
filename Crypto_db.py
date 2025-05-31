11eeec242022737ead2010a95679132c9a4a5882
Holuwercheyy <holuwercheyyhisserhah@gmail.com>
2025-05-31 22:00:21 +0100

    Crypto_db.py

---
+++ Crypto_db.py
@@ -0,0 +1,40 @@
+crypto_db = {
+    "Bitcoin": {
+        "price_trend": "rising",
+        "market_cap": "high",
+        "energy_use": "high",
+        "sustainability_score": 2/10
+    },
+    "Ethereum": {
+        "price_trend": "stable",
+        "market_cap": "high",
+        "energy_use": "medium",
+        "sustainability_score": 5/10
+    },
+    "Cardano": {
+        "price_trend": "rising",
+        "sustainability_score": 0.9
+    },
+    "Solana": {
+        "price_trend": "rising",
+        "market_cap": "medium",
+        "energy_use": "low",
+        "sustainability_score": 7/10
+    },
+    "Polkadot": {
+        "price_trend": "falling",
+        "market_cap": "medium",
+        "energy_use": "medium",
+        "sustainability_score": 6/10
+    },
+    "Algorand": {
+        "price_trend": "stable",
+        "market_cap": "low",
+        "energy_use": "low",
+        "sustainability_score": 8/10
+    },
+    "Ripple (XRP)": {
+        "price_trend": "rising",
+        "sustainability_score": 0.8
+    },
+}
